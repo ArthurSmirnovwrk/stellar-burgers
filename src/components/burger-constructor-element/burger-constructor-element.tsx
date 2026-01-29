4c9b1307cb72ab796f68413f0de5dc04328cb007
Arthur Smirnov <aakterhall@yandex.ru>
2026-01-29 14:09:42 +0300

    Исправил функционал перемещения элемента

--- src/components/burger-constructor-element/burger-constructor-element.tsx
+++ src/components/burger-constructor-element/burger-constructor-element.tsx
@@ -2,15 +2,36 @@ import { FC, memo } from 'react';
 import { BurgerConstructorElementUI } from '@ui';
 import { BurgerConstructorElementProps } from './type';
 import { useDispatch } from '../../services/store';
-import { removeIngredient } from '../../services/slices/constructorSlice';
+import {
+  removeIngredient,
+  moveIngredient
+} from '../../services/slices/constructorSlice';
 
 export const BurgerConstructorElement: FC<BurgerConstructorElementProps> = memo(
   ({ ingredient, index, totalItems }) => {
     const dispatch = useDispatch();
 
-    const handleMoveDown = () => {};
+    const handleMoveUp = () => {
+      if (index === 0) return;
 
-    const handleMoveUp = () => {};
+      dispatch(
+        moveIngredient({
+          fromIndex: index,
+          toIndex: index - 1
+        })
+      );
+    };
+
+    const handleMoveDown = () => {
+      if (index === totalItems - 1) return;
+
+      dispatch(
+        moveIngredient({
+          fromIndex: index,
+          toIndex: index + 1
+        })
+      );
+    };
 
     const handleClose = () => {
       dispatch(removeIngredient(ingredient.id));
